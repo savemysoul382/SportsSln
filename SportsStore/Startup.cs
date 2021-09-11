f9c9d582fa4b2391a2e7cd8f7dacd0364a0471ec
savemysoul382 <serjke88@gmail.com>
2021-09-11 20:10:13 +0300

    Additional data; first configuration and xunit project for tests

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -16,6 +16,7 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,16 @@ namespace SportsStore
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
+            app
+                .UseEndpoints(endpoints =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    endpoints.MapDefaultControllerRoute();
                 });
-            });
         }
     }
 }
